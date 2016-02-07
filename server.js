340b15e3da70e25df73a3eb3950253500daa7c7a
Dharini Ramakrishnan <dharini.ramakrishnan@gmail.com>
2016-02-06 22:55:50 -0800

    Post for todos

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 
 
-var todos = [ {
-	id: 1,
-	description: 'Meet for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},
-{
-	id:3,
-	description:'Go for walk',
-	completed: true
-}];
+var todos = [];
+
+var todoNextId = 1;
+app.use(bodyParser.json());
 
 app.get('/', function (req,res) {
 	res.send('ToDo API root');
@@ -41,14 +32,22 @@ app.get('/todos/:id', function (req,res) {
 		res.status(404).send();
 	}
 
-
-
 });
 
 app.get('/todos', function(req,res) {
 	res.json(todos);
 });
 
+//POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	body.id = todoNextId;
+	todoNextId++;
+	todos.push(body);
+
+
+	res.json(body);
+});
 
 
 
